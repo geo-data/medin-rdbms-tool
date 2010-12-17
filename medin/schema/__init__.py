685e68ef0fad5bd65fec4ec49bf32e47fec5847b
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2010-12-17 12:51:24 +0000

    Check the schema of the database
    Perform a test of the actual database schema that the user is
    connecting to ensure it conforms to that specified as a tool option
    (currently only the medin schema is available as a tool option).

--- medin/schema/__init__.py
+++ medin/schema/__init__.py
@@ -20,6 +20,51 @@ class Session(object):
         Sess = sessionmaker(bind=engine)
         self.sess = Sess()
 
+    def verifySchema(self):
+        """
+        Check the database schema matches expectations
+
+        Reflect the database schema and check it against the plugin
+        schema.
+        """
+        from sqlalchemy import MetaData
+        
+        errors = []
+        my_schema = self.getSchema()
+        db_schema = MetaData(self.sess.bind)
+        db_schema.reflect()
+
+        # iterate over each schema table
+        for my_table in my_schema.tables.values():
+            # ensure the table exists in the database
+            try:
+                db_table = db_schema.tables[my_table.name]
+            except KeyError:
+                errors.append('The table does not exist in the database: '+my_table.name)
+                continue
+
+            # ensure each column exists
+            for my_column in my_table.columns:
+                try:
+                    db_column = db_table.columns[my_column.name]
+                except KeyError:
+                    errors.append('The column does not exist in the table "%s": %s' % (db_table.name, my_column.name))
+                    continue
+            
+        return errors
+
+    def getSchema(self):
+        """
+        Return a sqlalchemy.MetaData object representing the database schema
+        """
+        raise NotImplementedError('This method must be overridden')
+
+    def setMapping(self):
+        """
+        Map the Metadata classes to the provider database schema
+        """
+        raise NotImplementedError('This method must be overridden')
+
     def getMetadataById(self, id):
         """
         A stub providing access to individual metadata objects
