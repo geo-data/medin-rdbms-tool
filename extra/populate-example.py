884a2f5dfc6c30cbebc22795e00a2bf6ed105c5b
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2011-06-02 10:50:54 +0100

    Update the copy database script
    The script now supports copying the example schema from one database
    to any other.

--- extra/populate-example.py
+++ extra/populate-example.py
@@ -1,43 +1,67 @@
+# Inspired by http://www.tylerlesmann.com/2009/apr/27/copying-databases-across-platforms-sqlalchemy/
 # ensure we're using UTF-8
 import sys
 sys = reload(sys)
 sys.setdefaultencoding('utf-8')
 
-import medin.provider.example
-import medin.vocabulary
-vocabs = medin.vocabulary.Session()
-s = medin.provider.example.Session('test', vocabs)
-sch = s.getSchema()
-
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+def make_session(connection_string):
+    engine = create_engine(connection_string, echo=False, convert_unicode=True)
+    Session = sessionmaker(bind=engine)
+    return Session(), engine
+
+def quick_mapper(table):
+    Base = declarative_base()
+    class GenericMapper(Base):
+        __table__ = table
+    return GenericMapper
+
+import medin.vocabulary
+import medin.contact
+
 from os.path import join, dirname, abspath
-dbname = abspath(join(dirname(medin.__file__),'data','example.sqlite'))
-print "Populating %s" % dbname
 
-e = create_engine('sqlite:///'+dbname)
-sch.bind = e
-sch.drop_all()
-sch.create_all()
+src_connstr = 'sqlite:///'+abspath(join(dirname(medin.__file__),'data','example.sqlite'))
+#dst_connstr = 'oracle://medin:mdip@localhost:1521/XE'
+#dst_connstr = 'mssql://medin:mdip@MEDIN'
+dst_connstr = 'mysql://homme:mdip@localhost/medin'
 
-import cx_Oracle
-c = cx_Oracle.connect('medin/mdip@localhost:1521/XE') # oracle connection
-db = e.raw_connection()                 # sqlite connection
+print "Copying %s to %s" % (src_connstr, dst_connstr)
 
-from itertools import repeat
-src = c.cursor()
-dst = db.cursor()
-for table_name in sch.tables:
-    print 'Copying %s' % table_name
-    columns = sch.tables[table_name].columns.keys()
-    colnames = ', '.join(columns)
-    sql_src = 'SELECT %s FROM %s' % (colnames, table_name)
-    #print sql_src
-    src.execute(sql_src)
-    
-    sql_dst = 'INSERT INTO %s (%s) VALUES (%s)' % (table_name, colnames, ','.join(repeat('?', len(columns))))
-    #print sql_dst
-    dst.executemany(sql_dst, src)
+source, src_e = make_session(src_connstr)
+destination, dst_e = make_session(dst_connstr)
 
-db.commit()
-dst.close()
-src.close()
+import medin.schema
+import imp
+schema_module = imp.load_module('medin', *imp.find_module('medin', medin.schema.__path__))
+
+vocabs = medin.vocabulary.Session()
+contacts = medin.contact.Session()
+s = schema_module.Session(src_e, vocabs, contacts)
+
+sch_src = s.getSchema()
+sch_src.bind = src_e
+
+print "Creating destination database schema"
+sch_dst = s.getSchema()
+sch_dst.bind = dst_e
+sch_dst.drop_all()
+sch_dst.create_all()
+
+for src_table in sch_src.sorted_tables:
+    print "Copying table %s" % src_table.name
+
+    NewRecord = quick_mapper(src_table)
+    columns = src_table.columns.keys()
+
+    for record in source.query(src_table).all():
+        data = dict(
+            [(str(column), getattr(record, column)) for column in columns]
+            )
+        destination.merge(NewRecord(**data))
+
+print "Committing"
+destination.commit()
