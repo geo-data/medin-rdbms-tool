geo-data/medin-rdbms-tool

commit d0fe3d856d2ecf7e2217bfb22b901855e11623ec

Embed term URL in keyword elements
Use the gmx:Anchor element within gmd:keyword elements where
appropriate. This element substitutes for gco:CharacterString and
works in the following way:

  <gmd:keyword>
    <gmx:Anchor xlink:href="http://vocab.ndg.nerc.ac.uk/term/P021/59/ZOOB">Zoobenthos taxonomy-related counts</gmx:Anchor>
  </gmd:keyword>

  instead of:

  <gmd:keyword>
    <gco:CharacterString>Zoobenthos taxonomy-related counts</gco:CharacterString>
  </gmd:keyword>

This change causes the resulting XML to fail the ISO/TS 19139:2007 XML
Schema validation.

This change re-implements the change in commit
be426850da7c8aa14020b4ec3c17e3111785c892

medin/metadata.py
@@ -716,7 +716,13 @@ class XMLBuilder(object):
             # add the thesaurus terms
             for term in terms:
                 keyword = MD_Keywords.newChild(None, 'keyword', None)
-                CharacterString = keyword.newChild(self.ns['gco'], 'CharacterString', escape(term.getTerm()))
+                try:
+                    url = term.key
+                except AttributeError:
+                    CharacterString = keyword.newChild(self.ns['gco'], 'CharacterString', escape(term.term))
+                else:
+                    Anchor = keyword.newChild(self.ns['gmx'], 'Anchor', escape(term.term))
+                    Anchor.setNsProp(self.ns['xlink'], 'href', escape(str(url)))
 
             thesaurusName = MD_Keywords.newChild(None, 'thesaurusName', None)
             thesaurusName.addChild(self.thesaurusToXML(thesaurus))

commit f9c9cb250ec7dad6d5be06ef076167d48e3dd9d1

Fix bug introduced in commit d0fe3d856d2ecf7e2217bfb22b901855e11623ec
This fix did not properly extract terms from keywords.

medin/metadata.py
@@ -744,9 +744,9 @@ class XMLBuilder(object):
                 try:
                     url = term.key
                 except AttributeError:
-                    CharacterString = keyword.newChild(self.ns['gco'], 'CharacterString', escape(term.term))
+                    CharacterString = keyword.newChild(self.ns['gco'], 'CharacterString', escape(term.getTerm()))
                 else:
-                    Anchor = keyword.newChild(self.ns['gmx'], 'Anchor', escape(term.term))
+                    Anchor = keyword.newChild(self.ns['gmx'], 'Anchor', escape(term.getTerm()))
                     Anchor.setNsProp(self.ns['xlink'], 'href', escape(str(url)))
 
             thesaurusName = MD_Keywords.newChild(None, 'thesaurusName', None)